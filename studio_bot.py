8f3c2077f6eed001b2ffa6bbb7f06881b6e6187c
Harrison John <hjohn@qore.inc>
2025-10-10 21:21:36 +0100

    fixed minor issues

--- studio_bot.py
+++ studio_bot.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route("/chat")
 def chat():
-    args = request.get_json()
-    message = args["message"]
-    print("message")
+    message = request.args.get("message")
+    print(message)
+    return jsonify({"message": message})
 
 if __name__ == "__main__":
     app.run()
